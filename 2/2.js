46ef69c2970e735ba1e8a3010fe225e868991218
Henk te Sligte <htesligte@gmail.com>
2021-12-02 08:30:54 +0100

    day 2

---
+++ 2/2.js
@@ -0,0 +1,27 @@
+const fs = require('fs/promises');
+const main = async () => {
+    const contents = await fs.readFile('input.txt');
+    const lines = contents.toString().split('\n');
+    const instructions = lines.map(line => line.split(' '));
+    const pos = {depth: 0, forward: 0, aim: 0};
+    for (const instruction of instructions) {
+        switch(instruction[0]) {
+            case 'forward': 
+                pos.forward += parseInt(instruction[1]);
+                pos.depth += pos.aim * parseInt(instruction[1]);
+                break;
+            
+            case 'down': 
+                //pos.depth += parseInt(instruction[1]);
+                pos.aim += parseInt(instruction[1]);
+                break;
+            
+            case 'up':
+                //pos.depth -= parseInt(instruction[1]);
+                pos.aim -= parseInt(instruction[1]);
+                break;
+        }
+    }
+    console.log(pos.depth * pos.forward);
+}
+main();
